1dcbd1784bf461959bb4b12cec6a05654b51f605
KazukiKanaya-0106 <kanaya.kazuki0106@gmail.com>
2026-07-20 17:30:09 +0900

    feat: add public_key.go

---
+++ internal/sshkey/public_key.go
@@ -0,0 +1,52 @@
+package sshkey
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/ssh"
+)
+
+type PublicKey struct {
+	key     ssh.PublicKey
+	comment string
+}
+
+func Parse(input string) (PublicKey, error) {
+	input = strings.TrimSpace(input)
+
+	if input == "" {
+		return PublicKey{}, fmt.Errorf("public key must not be empty")
+	}
+
+	if strings.Contains(input, "PRIVATE KEY") {
+		return PublicKey{}, fmt.Errorf(
+			"private keys must not be submitted",
+		)
+	}
+
+	key, comment, options, _, err := ssh.ParseAuthorizedKey([]byte(input))
+	if err != nil {
+		return PublicKey{}, fmt.Errorf(
+			"parse SSH public key: %w",
+			err,
+		)
+	}
+
+	if len(options) > 0 {
+		return PublicKey{}, fmt.Errorf(
+			"provided exactly one public key",
+		)
+	}
+
+	return PublicKey{
+		key:     key,
+		comment: comment,
+	}, nil
+}
+
+func (k PublicKey) Equal(other PublicKey) bool {
+	return k.key.Type() == other.key.Type() &&
+		bytes.Equal(k.key.Marshal(), other.key.Marshal())
+}
